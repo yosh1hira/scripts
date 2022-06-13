25a8001bf91eeb3f299956ab88eb729c2ea32a84
yoshihira <3223266+yosh1hira@users.noreply.github.com>
2022-06-13 17:05:54 +0900

    -

--- brew_update.sh
+++ brew_update.sh
@@ -2,13 +2,13 @@
 
 log_name=/tmp/brewlog/`date "+%Y%m%d_%H%M%S"`.log
 
-echo 'brew doctor ----------------------------' >> {log_name} 2>&1
+echo 'brew doctor ----------------------------' >> ${log_name} 2>&1
 brew doctor >> ${log_name} 2>&1
 if [[ $? -ne 0 ]]; then
   brew update >> ${log_name} 2>&1
-  echo 'brew update ----------------------------' >> {log_name} 2>&1
+  echo 'brew update ----------------------------' >> ${log_name} 2>&1
   brew upgrade >> ${log_name} 2>&1
-  echo 'brew upgrade ----------------------------' >> {log_name} 2>&1
+  echo 'brew upgrade ----------------------------' >> ${log_name} 2>&1
   brew cleanup >> ${log_name} 2>&1
-  echo 'brew cleanup ----------------------------' >> {log_name} 2>&1
+  echo 'brew cleanup ----------------------------' >> ${log_name} 2>&1
 fi
